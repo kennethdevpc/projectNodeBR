20c8f36ea61c393a1ed0a26c33674b9a117ddb3a
kennethdevpc <kennethdevpc@gmail.com>
2023-02-17 16:58:35 -0500

    express-validator, y validacion de usuarios repetidos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,33 +1,61 @@
-import Usuario from "../models/Usuario.js";
+import Usuario from '../models/Usuario.js';
+import { check, validationResult } from 'express-validator';
+import { where } from 'sequelize';
 const formularioLogin = (req, res) => {
   //req: lo que le mando al servidor de node
   //res: lo que responde el servidor de node
-  res.render("auth/login", {
-    pagina: "inicion Sesión",
+  res.render('auth/login', {
+    pagina: 'inicion Sesión',
   });
 };
 
 const formularioRegistro = (req, res) => {
-  res.render("auth/registro", {
-    pagina: "crear cuenta",
+  res.render('auth/registro', {
+    pagina: 'crear cuenta',
   });
 };
 const registrar = async (req, res) => {
-  const usuario = await Usuario.create(req.body);
+  await check('nombre').notEmpty().withMessage('Nombre no puede ir vacio ').run(req);
+  await check('email').isEmail().withMessage('Debe colocar un email valido ').run(req);
+  await check('password').isLength({ min: 6 }).withMessage('password corta ').run(req);
+  await check('repetir_password').equals(req.body.password).withMessage('password diferente?').run(req);
+  let resultado = validationResult(req);
 
-  res.json(usuario);
+  if (!resultado.isEmpty()) {
+    return res.render('auth/registro', {
+      pagina: 'crear cuenta',
+      errores: resultado.array(),
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email,
+      },
+    });
+  }
+
+  const { nombre, email, password } = req.body;
+  const existeUsuario = await Usuario.findOne({ where: { email } });
+
+  if (existeUsuario) {
+    return res.render('auth/registro', {
+      pagina: 'crear cuenta',
+      errores: [{ msg: 'El usuario esta registrado' }],
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email,
+      },
+    });
+  }
+  //return;
+  //const usuario = await Usuario.create(req.body);
+
+  //res.json(usuario);
 
   // res.json({ mensaje: "se hizo post", data: req.data });
 };
 const formularioOlvidePassword = (req, res) => {
-  res.render("auth/olvide-password", {
-    pagina: "Recuperacion de acceso a bienes raices",
+  res.render('auth/olvide-password', {
+    pagina: 'Recuperacion de acceso a bienes raices',
   });
 };
 
-export {
-  formularioLogin,
-  formularioRegistro,
-  formularioOlvidePassword,
-  registrar,
-};
+export { formularioLogin, formularioRegistro, formularioOlvidePassword, registrar };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //const express = require("express"); //CommonJS modules
-import express from "express"; // ECMAScript modules
+import express from 'express'; // ECMAScript modules
 //importando rutas
 //forma1:
-import usuarioRoutes from "./routes/usuarioRoutes.js";
+import usuarioRoutes from './routes/usuarioRoutes.js';
 //forma2: import router from "./routes/usuarioRoutes.js";
-import db from "./config/db.js";
+import db from './config/db.js';
 
 //crear app-----------------------------
 const app = express();
@@ -13,27 +13,27 @@ app.use(express.urlencoded({ extended: true }));
 //conexion a base de datos:-----------------------------
 try {
   await db.authenticate();
-  await db.sync({ force: true });
-  console.log("Connection database kenneth has been established successfully.");
+  await db.sync({ force: false });
+  console.log('Connection database kenneth has been established successfully.');
 } catch (error) {
-  console.error("Unable to connect to the database:", error);
+  console.error('Unable to connect to the database:', error);
 }
 
 //pug-----------------------------
-app.set("view engine", "pug");
-app.set("views", "./views");
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 //Carpeta Publica-----------------------------
-app.get("/", (req, res) => {
-  res.json({ mensaje: "hola mundo en expres2 con post" });
+app.get('/', (req, res) => {
+  res.json({ mensaje: 'hola mundo en expres2 con post' });
 });
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //routing-----------------------------
-app.use("/auth", usuarioRoutes);
+app.use('/auth', usuarioRoutes);
 
 //deinir un puerto y arrancarlo-----------------------------
-const port = 3200;
+const port = 3300;
 
 app.listen(port, () => {
   console.log(`el servidor esta en ${port}`);
