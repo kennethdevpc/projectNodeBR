014bcf32c437452cc3bbad29efc4047fe09d2421
kennethdevpc <kennethdevpc@gmail.com>
2024-05-30 19:50:11 -0500

    creacion de seeder que elimina y crea datos en las tablas

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,9 +1,10 @@
 // import {exit} from 'node:process' //forma 2 de uso de exit
 import categorias from "./categorias.js"; //puedo importarlo con un nombre cualqueira ya que es un export default
+import precios from './precios.js';
 import Categoria from '../models/Categoria.js';
+import Precio from '../models/Precio.js';
+
 import db from "../config/db.js";
-import precios from "./precios.js";
-import Precio from "../models/Precio.js";
 
 const importarDatos = async () => {
   try {
@@ -14,8 +15,9 @@ const importarDatos = async () => {
     await db.sync()
 
     //Insertamos los datos
-    await Categoria.bulkCreate(categorias)  //le paso al modelo esas categorias creadas en el arreglo de objeto 
-    await Precio.bulkCreate(precios)  //le paso al modelo esas categorias creadas en el arreglo de objeto 
+    const a=  Categoria.bulkCreate(categorias)  //le paso al modelo esas categorias creadas en el arreglo de objeto 
+    const b=  Precio.bulkCreate(precios)  //le paso al modelo esas categorias creadas en el arreglo de objeto 
+    await Promise.all([a,b])
     console.log("Datos Importados correctamente")
     
   } catch (error) {
@@ -24,6 +26,25 @@ const importarDatos = async () => {
     // exit(1); //forma 2 de uso de exit
   }
 }
+
+const eliminarDatos = async () => {
+  try {
+    await Promise.all([
+      Categoria.destroy({ where: {}, truncate: true }),
+      Precio.destroy({ where: {}, truncate: true }),
+    ]);
+    console.log("Datos eliinados correctamente")
+    process.exit();
+
+    
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+    
+  }
+}
+
+
 /*
 process: es un proceso intero de node
 argv: es algo inerno de node. //ver pasos2024 35.3.2
@@ -33,3 +54,8 @@ if (process.argv[2] === "-i") {
   importarDatos();
 
 }
+if (process.argv[2] === "-e") {   
+  eliminarDatos();
+
+}
+
